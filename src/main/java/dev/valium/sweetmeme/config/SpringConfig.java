002d903f2346fde05de4d0045e1a000288cb9b25
Valium-K <lschan96@gmail.com>
2021-09-02 22:03:02 +0900

    스프링 설정 추가

--- src/main/java/dev/valium/sweetmeme/config/SpringConfig.java
+++ src/main/java/dev/valium/sweetmeme/config/SpringConfig.java
@@ -1,14 +1,21 @@
 package dev.valium.sweetmeme.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Locale;
 import java.util.Optional;
 import java.util.UUID;
 
 @Configuration
+@EnableJpaAuditing
 public class SpringConfig {
 
     /**
@@ -23,5 +30,24 @@ public class SpringConfig {
     @Bean
     public static BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public MessageSource messageSource() {
+        var messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(3);
+
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.US);
+
+        return sessionLocaleResolver;
     }
 }
