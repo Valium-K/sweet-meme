0737107955e098085f347db0620735a152b0cb78
Valium-K <lschan96@gmail.com>
2021-09-02 22:01:36 +0900

    베이스 엔티티 추가

--- src/main/java/dev/valium/sweetmeme/domain/BaseEntityTime.java
+++ src/main/java/dev/valium/sweetmeme/domain/BaseEntityTime.java
@@ -10,16 +10,24 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-public abstract class BaseEntity extends BaseEntityTime {
+public abstract class BaseEntityTime {
 
-    @CreatedBy
+    @CreatedDate
     @Column(updatable = false)
-    private String createdBy;
+    private LocalDateTime createdDate;
 
-    @LastModifiedBy
-    private String lastModifiedBy;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+//    @CreatedBy
+//    @Column(updatable = false)
+//    private String createdBy;
+//
+//    @LastModifiedBy
+//    private String lastModifiedBy;
 }
